388c904a12d5d45881026111f137684ecf6dd6dd
Mert Salık <53333294+codermert@users.noreply.github.com>
2024-08-20 23:36:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,27 +12,37 @@ fetch(url)
     const $ = cheerio.load(body);
     const flightData = [];
 
-    $('ul.flights-list li.flight').each((index, element) => {
+    $('li.flight').each((index, element) => {
       let price = $(element).find('.price').text().trim();
       const airline = $(element).find('img').attr('alt');
       const timeRange = $(element).find('.reduced').text().trim();
       let date = $(element).find('.text-muted').first().text().trim();
-      let duration = $(element).find('.col-xs-4 .row .col-md-12').first().text().trim();
-      const route = $(element).find('.col-xs-4 .row .col-md-12').last().text().trim();
-      const direct = $(element).find('.col-xs-1').text().trim();
+      let duration = $(element).find('.col-xs-5.col-sm-5.col-md-5').first().text().trim().split('\n')[0].trim();
+      const route = $(element).find('.col-xs-5.col-sm-5.col-md-5').find('.text-muted').text().trim();
+      const direct = $(element).find('.col-xs-2.col-sm-2.col-md-2.px-0').text().trim();
 
       // Fiyatı USD olarak ayarla
       price = `${price} USD`;
 
       // Tarihi Türkçe'ye çevir
-      const parsedDate = parse(date, 'EEE MMM do', new Date());
-      date = format(parsedDate, 'd MMMM yyyy, EEEE', { locale: tr });
+      let parsedDate;
+      try {
+        parsedDate = parse(date, 'EEE MMM do yyyy', new Date());
+        date = format(parsedDate, 'd MMMM yyyy, EEEE', { locale: tr });
+      } catch (error) {
+        console.error('Tarih parse edilirken hata oluştu:', error);
+        date = 'Geçersiz tarih';  // or any default/fallback value
+      }
 
       // Süreyi Türkçe'ye çevir
       const durationParts = duration.match(/(\d+)h\s+(\d+)m/);
-      const hours = parseInt(durationParts[1], 10);
-      const minutes = parseInt(durationParts[2], 10);
-      duration = `${hours} saat ${minutes} dakika`;
+      if (durationParts) {
+        const hours = parseInt(durationParts[1], 10);
+        const minutes = parseInt(durationParts[2], 10);
+        duration = `${hours} saat ${minutes} dakika`;
+      } else {
+        duration = 'Geçersiz süre';  // or any default/fallback value
+      }
 
       flightData.push({
         fiyat: price,
